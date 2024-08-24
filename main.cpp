8f313a8d436fc8a0f25094b496f63218f662c771
chenziang <3538995003@qq.com>
2024-08-24 19:38:07 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <cstdio>
+#include <queue>
+#include <cstring>
+#include <cassert>
+#include <algorithm>
+#include <sstream>
+#include <random>
+#include <stack>
+#include <set>
+#include <list>
+#include <thread>
+
+
+using namespace std;
+const int MAX = 200010;
+const long long inf = (1ll << 60);
+typedef long long ll;
+
+int main() {
+    cout << "Hello, World! " << endl;
+
+
+
+    return 0;
+}
+
