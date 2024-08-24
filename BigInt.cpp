e2786d6e26ef4a321a9420d4d799b443462c8c8e
chenziang <3538995003@qq.com>
2024-08-24 19:40:43 +0800

    feat(bigint): 添加BigInt类及其头文件和源文件
    BigInt类已经作为一个新的功能添加到项目中，包括BigInt.h头文件和BigInt.cpp源文件的创建。该类目前为一个空类，尚未实现具体功能。

---
+++ BigInt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 陈子昂 on 2024/8/24.
+//
+
+#include "BigInt.h"
---
+++ BigInt.h
@@ -0,0 +1,14 @@
+//
+// Created by 陈子昂 on 2024/8/24.
+//
+
+#ifndef BIGINT_BIGINT_H
+#define BIGINT_BIGINT_H
+
+
+class BigInt {
+
+};
+
+
+#endif //BIGINT_BIGINT_H
